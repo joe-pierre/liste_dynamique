92557004a0709905bdc5d153850aef60f3a2c8dc
Joseph Pierre Gomis <gomisjoepierre@gmail.com>
2022-03-03 13:14:34 +0000

    Add validation constraints

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,29 +6,35 @@ use App\Entity\City;
 use App\Entity\Country;
 use App\Repository\CityRepository;
 use App\Repository\CountryRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function home(): Response
+    public function home(Request $request): Response
     {
         $form = $this->createFormBuilder()
-            ->add('name', TextType::class)
+            ->add('name', TextType::class, [
+                'constraints' => new NotBlank(),
+            ])
             ->add('country', EntityType::class, [
                 'class' => Country::class,
                 'choice_label' => 'name',
                 'placeholder' => 'Please choose a country',
                 'query_builder' => function (CountryRepository $countryRepo) {
                     return $countryRepo->createQueryBuilder('c')->orderBy('c.name', 'ASC');
-                }
+                },
+                'constraints' => new NotBlank(['message'=>'Please choose a country']),
             ])
             ->add('city', EntityType::class, [
                 'class' => City::class,
@@ -37,11 +43,24 @@ class HomeController extends AbstractController
                 'disabled' => true,
                 'query_builder' => function (CityRepository $cityRepo) {
                     return $cityRepo->createQueryBuilder('c')->orderBy('c.name', 'ASC');
-                }
+                },
+                'constraints' => new NotBlank(['message'=>'Please choose a city']),
             ])
-            ->add('message', TextareaType::class)
-            ->getForm();
+            ->add('message', TextareaType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 5])
+                ]
+            ])
+            ->getForm()
+        ;
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd('cool');
+        }
+        
         return $this->renderForm('layouts/home.html.twig', [
             'form' => $form,
         ]);
